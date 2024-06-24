eb3ad457038a9ace9392d0bbd9f0b7ceaee2a7ff
Gabriel García <89267342+gabito1966@users.noreply.github.com>
2024-06-24 11:54:22 -0300

    Update TodoApp.tsx

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -19,7 +19,7 @@ export const TodoApp = () => {
     return (
         <div>
             <h1>Lista de tareas</h1>
-            <div>
+            <div className="flex">
                 <input
                     type="text"
                     value={nuevaTarea}
